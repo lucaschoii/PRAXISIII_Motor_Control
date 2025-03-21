434cc8db0386a1ac80ecede3c8b18e524da29902
lucaschoii <choi.lucas.945@gmail.com>
2025-03-20 21:59:30 -0400

    variable rename

--- motor_logic.py
+++ motor_logic.py
@@ -9,7 +9,7 @@ from ForceSensor import ForceSensor
 motor = Motor()
 force_sensor = ForceSensor()
 
-obstacle_steps = []
+obstacles = []
 
 
 def hit(action):
@@ -31,16 +31,16 @@ while True:
         
         print("Obstacle detected! Avoiding...")
 
-        if len(obstacle_steps) == 0:
+        if len(obstacles) == 0:
             motor.reverse()
             motor.tank_right()
             if hit(motor.forward): 
-                obstacle_steps.append('object')
+                obstacles.append('object')
                 continue
             if hit(motor.tank_left): continue
             if hit(motor.forward): continue
         
-        elif obstacle_steps[-1] == 'object':
+        elif obstacles[-1] == 'object':
             print("Second obstacle detected! Adjusting route...")
             motor.reverse()
             motor.tank_right()
@@ -51,41 +51,41 @@ while True:
             motor.forward()
             motor.tank_left()
             if hit(motor.forward): 
-                obstacle_steps.append('fence')
+                obstacles.append('fence')
                 continue
             motor.tank_left()
             if hit(motor.forward): 
-                obstacle_steps.append('fence')
+                obstacles.append('fence')
                 continue
             motor.tank_left()
             if hit(motor.forward): 
-                obstacle_steps.append('fence')
+                obstacles.append('fence')
                 continue
 
             # at this point, we have cleared the latest object
-            obstacle_steps.pop()
+            obstacles.pop()
             motor.tank_right()
 
             if hit(motor.forward): 
-                obstacle_steps.append('object')
+                obstacles.append('object')
                 continue
 
             motor.tank_left()
             if hit(motor.forward): 
-                obstacle_steps.append('fence')
+                obstacles.append('fence')
                 continue
 
             motor.tank_right()
             #now we have cleared the previous object as well
-            obstacle_steps.pop()
+            obstacles.pop()
             continue
 
-        elif obstacle_steps[-1] == 'fence':  # If we hit the same object twice -> fence detected
+        elif obstacles[-1] == 'fence':  # If we hit the same object twice -> fence detected
             print("Fence detected! Reversing direction...")
             motor.reverse()
             motor.tank_right()
             motor.tank_right()
-            obstacle_steps.clear() # do not care about other objects now, just go back the other way
+            obstacles.clear() # do not care about other objects now, just go back the other way
 
 
         
